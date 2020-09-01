66eb2151066444276cc02d0c1877121f315bf03d
Tom Tarpey <tomtarpeydev@gmail.com>
2020-09-01 22:39:49 +0100

    introI and II starter

---
+++ IntroI/01_intro_collections.py
@@ -0,0 +1,68 @@
+
+# collections
+
+# create an empty list? Array
+# my_list = []
+# my_list2 = list()
+
+# print(my_list)
+
+# create a list with numbers 1, 2, 3, 4, 5
+
+# add an element 24 to lst1
+
+# add an element 12 to the start of lst1
+
+# print(lst1)
+
+# # print all values in lst2
+# print(lst2)
+# print(lst1[0])
+# print(lst1[1])
+# print(lst1[2])
+# print(lst1[3])
+# print(lst1[4])
+
+# loop over the list using a for loop
+
+
+
+# while loop
+
+
+# List Comprehensions
+
+# Create a new list containing the squares of all values in 'numbers'
+numbers = [1, 2, 3, 4]
+squares = []
+# print(numbers)
+# print(squares)
+# Filtering with a list comprehension
+evens = []
+
+# create a new list of even numbers using the values of the numbers list as inputs
+# print(evens)
+
+# create a new list containing only the names that start with 's' make sure they are capitalized (regardless of their original case)
+names = ["Sarah", "jorge", "sam", "frank", "bob", "sandy"]
+s_names = []
+# print(s_names)
+
+
+
+
+# Dictionaries
+
+# Create a new dictionary
+
+# empty
+
+# key value pairs
+
+# access an element via its key
+
+
+
+# Lets think about Tuples?
+# this of an imutable list --> the tuple
+# good for constant values

5a21fddbb23f987304d7dbe49f0e730fd3a023a6
Tom Tarpey <tomtarpeydev@gmail.com>
2020-09-04 04:38:16 +0100

    end of intro2

--- IntroII/02_guessing_game.py
+++ IntroII/02_guessing_game.py
@@ -5,3 +5,16 @@
 # if they are the same print player wins
 # if they are different print guess again
 # and loop
+
+number = 45
+playing = True
+# REPL
+while playing: # LOOP
+    user_guess = int(input(" Guess the number I am thinking of >>> ")) # READ
+
+    # EVAL
+    if number == user_guess:
+        print("You Win!")  # PRINT
+        playing = False
+    else:
+        print("Not correct, guess again!")
